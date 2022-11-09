f6ae14c4e3ea7c2a7c6b9d2dbcd8301328f30dbb
GabDSOliveira <gabriel.dsoliveira@mercadolivre.com>
2022-11-09 11:24:23 -0300

    Atualização dos repositorios e serviços

---
+++ src/main/java/com/grupo6/projetointegrador/repository/SectionRepo.java
@@ -0,0 +1,7 @@
+package com.grupo6.projetointegrador.repository;
+
+import com.grupo6.projetointegrador.model.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectionRepo extends JpaRepository<Section, Long> {
+}
--- src/main/java/com/grupo6/projetointegrador/service/InboundOrderServiceImpl.java
+++ src/main/java/com/grupo6/projetointegrador/service/InboundOrderServiceImpl.java
@@ -1,11 +1,15 @@
 package com.grupo6.projetointegrador.service;
 
 import com.grupo6.projetointegrador.dto.CreateInboundOrderDto;
+import com.grupo6.projetointegrador.dto.CreateItemBatchDto;
 import com.grupo6.projetointegrador.dto.ItemBatchDto;
+import com.grupo6.projetointegrador.exception.BusinessRuleException;
 import com.grupo6.projetointegrador.exception.NotFoundException;
+import com.grupo6.projetointegrador.model.Section;
 import com.grupo6.projetointegrador.model.Warehouse;
 import com.grupo6.projetointegrador.model.WarehouseOperator;
 import com.grupo6.projetointegrador.repository.InboundOrderRepo;
+import com.grupo6.projetointegrador.repository.SectionRepo;
 import com.grupo6.projetointegrador.repository.WarehouseOperatorRepo;
 import com.grupo6.projetointegrador.repository.WarehouseRepo;
 import org.springframework.stereotype.Service;
@@ -17,30 +21,57 @@ public class InboundOrderServiceImpl implements InboundOrderService{
 
     private InboundOrderRepo inboundOrderRepo;
     private WarehouseRepo warehouseRepo;
-
     private WarehouseOperatorRepo warehouseOperatorRepo;
+    private SectionRepo sectionRepo;
 
-    public InboundOrderServiceImpl(InboundOrderRepo inboundOrderRepo, WarehouseRepo warehouseRepo, WarehouseOperatorRepo warehouseOperatorRepo) {
+    public InboundOrderServiceImpl(InboundOrderRepo inboundOrderRepo, WarehouseRepo warehouseRepo, WarehouseOperatorRepo warehouseOperatorRepo, SectionRepo sectionRepo) {
         this.inboundOrderRepo = inboundOrderRepo;
         this.warehouseRepo = warehouseRepo;
         this.warehouseOperatorRepo = warehouseOperatorRepo;
+        this.sectionRepo = sectionRepo;
     }
 
     @Override
     public List<ItemBatchDto> createInboundOrder(CreateInboundOrderDto createInboundOrderDto) {
-
+        return null;
     }
 
-    private Warehouse checkWarehouseId(Long warehouseId){
+    private Warehouse findOrThrowWarehouse(Long warehouseId){
         return warehouseRepo.findById(warehouseId).orElseThrow(() -> new NotFoundException("Armazém não encontrado."));
     }
 
-    private WarehouseOperator checkWarehouseOperator(Long warehouseOperatorId, Long warehouseId){
+    private WarehouseOperator findOrThrowWarehouseOperator(Long warehouseOperatorId, Long warehouseId){
         WarehouseOperator operator = warehouseOperatorRepo.findById(warehouseOperatorId).orElseThrow(() -> new NotFoundException("Operador não encontrado."));
-        if(checkWarehouseId(warehouseId).getWarehouseOperator().getId().equals(warehouseOperatorId)){
+
+        if(findOrThrowWarehouse(warehouseId).getWarehouseOperator().getId().equals(warehouseOperatorId)){
             return operator;
         } else {
-            throw new
+            throw new BusinessRuleException("Este operador não faz parte do armazém.");
         }
+    }
+
+    private Section findOrThrowSection(Long sectionId){
+        return sectionRepo.findById(sectionId).orElseThrow(() -> new NotFoundException("Seção não encontrada."));
+    }
+
+    private Section verifyWarehouseMatchSection(Long sectionId, Long warehouseId){
+        Section section = findOrThrowSection(sectionId);
+        if(section.getWarehouse().getId().equals(warehouseId)){
+            return section;
+        } else {
+            throw new BusinessRuleException("Esta seção não faz parte do armazém.");
+        }
+    }
+
+    private boolean matchStorageType(CreateItemBatchDto itemBatchDto, Long sectionId){
+        Section section = findOrThrowSection(sectionId);
+
+        return section.getStorageType().equals(itemBatchDto.getStorageType());
+    }
+
+    private boolean verifyAvailableVolume(Long volume, Long sectionId){
+        Section section = findOrThrowSection(sectionId);
+
+        return section.getVolume().compareTo(volume) >= 0;
     }
 }
