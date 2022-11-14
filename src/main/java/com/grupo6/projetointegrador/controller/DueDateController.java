fe34d5ad0a7dd77fe794227013844aef0c432f2f
GabDSOliveira <gabriel.dsoliveira@mercadolivre.com>
2022-11-14 15:19:35 -0300

    Criado o controller, service e dto do Due-date(requisito 5)

---
+++ src/main/java/com/grupo6/projetointegrador/controller/DueDateController.java
@@ -0,0 +1,36 @@
+package com.grupo6.projetointegrador.controller;
+
+import com.grupo6.projetointegrador.dto.DueDateItemBatchDto;
+import com.grupo6.projetointegrador.service.DueDateService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/due-date")
+public class DueDateController {
+    private DueDateService dueDateService;
+
+    public DueDateController(DueDateService dueDateService) {
+        this.dueDateService = dueDateService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DueDateItemBatchDto>> findItemBatchBySection(
+            @RequestParam(name = "sectionId", required = true) Long sectionId,
+            @RequestParam(name = "days", required = true) int days){
+        return ResponseEntity.ok(dueDateService.findItemBatchBySection(sectionId, days));
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<DueDateItemBatchDto>> findItemBatchByCategory(
+            @RequestParam(name = "category", required = false) String category,
+            @RequestParam(name = "days", required = true) int days,
+            @RequestParam(name = "order", defaultValue = "ASC") String order){
+        return ResponseEntity.ok(dueDateService.findItemBatchByCategory(category, days, order));
+    }
+}
---
+++ src/main/java/com/grupo6/projetointegrador/service/DueDateServiceImpl.java
@@ -0,0 +1,40 @@
+package com.grupo6.projetointegrador.service;
+
+import com.grupo6.projetointegrador.dto.DueDateItemBatchDto;
+import com.grupo6.projetointegrador.exception.BusinessRuleException;
+import com.grupo6.projetointegrador.exception.NotFoundException;
+import com.grupo6.projetointegrador.model.enumeration.Category;
+import com.grupo6.projetointegrador.repository.ItemBatchRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DueDateServiceImpl implements DueDateService{
+    private ItemBatchRepo itemBatchRepo;
+
+    public DueDateServiceImpl(ItemBatchRepo itemBatchRepo) {
+        this.itemBatchRepo = itemBatchRepo;
+    }
+
+    @Override
+    public List<DueDateItemBatchDto> findItemBatchBySection(Long sectionId, int days) {
+        if (days < 0){
+            throw new BusinessRuleException("Intervalo de dias inválido. Precisa ser maior que zero.");
+        }
+
+        return itemBatchRepo.findByDueDateWithSectionId(sectionId, days).orElseThrow(() -> new NotFoundException("Nenhum lote encontrado."));
+    }
+
+    @Override
+    public List<DueDateItemBatchDto> findItemBatchByCategory(String category, int days, String order) {
+        try {
+            String categoryToSearch = Category.fromCode(category).getName();
+            return itemBatchRepo.findByDueDateWithCategory(categoryToSearch, days, order.toLowerCase())
+                    .orElseThrow(() -> new NotFoundException("Nenhum lote encontrado."));
+        } catch (NullPointerException exception){
+            throw new BusinessRuleException("A categoria passada é inválida.");
+        }
+
+    }
+}
