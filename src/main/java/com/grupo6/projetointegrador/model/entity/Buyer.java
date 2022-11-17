aebb428461528e32eb5cc3f82c8353937aac47e8
jleva <Sdisjkdsakld@ajdshfkjasdhf>
2022-11-17 10:56:28 -0300

    new database insert/ fix GenerationType entity (Buyer, OrderPurchase and ProductOrder)/ update collection postman/ add UML

--- src/main/java/com/grupo6/projetointegrador/model/entity/Buyer.java
+++ src/main/java/com/grupo6/projetointegrador/model/entity/Buyer.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class Buyer {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
   @OneToMany(mappedBy = "buyer")
--- src/main/java/com/grupo6/projetointegrador/model/entity/ProductOrder.java
+++ src/main/java/com/grupo6/projetointegrador/model/entity/ProductOrder.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class ProductOrder {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
