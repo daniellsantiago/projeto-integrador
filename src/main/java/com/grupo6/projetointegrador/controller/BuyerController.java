7ab3f9ece9f5989f63febf5a039bf59c1ccca83f
natashabusnardo <natasha.busnardo@mercadolivre.com>
2022-11-22 18:36:17 -0300

    feat: adicao requests http

--- src/main/java/com/grupo6/projetointegrador/controller/BuyerController.java
+++ src/main/java/com/grupo6/projetointegrador/controller/BuyerController.java
@@ -4,12 +4,10 @@ import com.grupo6.projetointegrador.dto.CreateBuyerDto;
 import com.grupo6.projetointegrador.model.entity.Buyer;
 import com.grupo6.projetointegrador.service.BuyerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,8 +21,23 @@ public class BuyerController {
 
     @PostMapping
     public ResponseEntity<Buyer> saveBuyer(@RequestBody @Valid CreateBuyerDto createBuyerDto) {
-        return ResponseEntity.ok(buyerService.save(createBuyerDto));
+        return new ResponseEntity<>(buyerService.save(createBuyerDto), HttpStatus.CREATED);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Buyer> findBuyer(@PathVariable Long id) {
+        return new ResponseEntity<>(buyerService.findById(id), HttpStatus.OK);
+    }
 
+    @PatchMapping("/{id}")
+    public ResponseEntity<Buyer> updateBuyer(@PathVariable Long id,
+                                             @RequestBody @Valid CreateBuyerDto createBuyerDto) {
+        return new ResponseEntity<>(buyerService.update(id, createBuyerDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBuyer(@PathVariable Long id) {
+        buyerService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
