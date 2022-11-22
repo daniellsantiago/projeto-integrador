be73498e15d4a9baa76eb6bc912bebe4937f1f3e
natashabusnardo <natasha.busnardo@mercadolivre.com>
2022-11-22 18:35:00 -0300

    fix: adicao notations notblank

--- src/main/java/com/grupo6/projetointegrador/dto/CreateBuyerDto.java
+++ src/main/java/com/grupo6/projetointegrador/dto/CreateBuyerDto.java
@@ -4,15 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateBuyerDto {
+    @NotBlank(message = "Nome é obrigatório")
     private String name;
+
+    @NotBlank(message = "CPF é obrigatório")
     private String cpf;
+
+    @NotBlank(message = "Endereço é obrigatório")
     private String address;
+
+    @NotBlank(message = "Bairro é obrigatório")
     private String neighborhood;
+
+    @NotBlank(message = "Cidade é obrigatório")
     private String city;
+
+    @NotBlank(message = "Estado é obrigatório")
     private String state;
+
+    @NotBlank(message = "CEP é obrigatório")
+    @Pattern(regexp = "^[0-9]*$", message = "CEP deve conter apenas números")
+    @Size(min = 8, max = 8)
     private String zipCode;
 }
